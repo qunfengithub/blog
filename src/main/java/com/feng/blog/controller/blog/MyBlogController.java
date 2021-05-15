d3642016d0cb5c84c7d7c8469d0f827fac5ab833
dqf <1448337033@qq.com>
2021-05-15 23:36:03 +0800

    注释的增加

--- src/main/java/com/feng/blog/controller/blog/MyBlogController.java
+++ src/main/java/com/feng/blog/controller/blog/MyBlogController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+//这个是博客的页面
 @Controller
 public class MyBlogController {
 
